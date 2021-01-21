1177a1607678607887862ac3073df3fe112d11d0
deblina23 <deblina.ghosh.kolkata@gmail.com>
2021-01-21 15:06:31 +0000

    console read added and code modified accordingly

--- PizzaPractice/readInput.py
+++ PizzaPractice/readInput.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 import itertools
-inputFile = input("Enter the File:")
-inputFile = open(inputFile,"r")
-firstLine = inputFile.readline()
+import sys
+inpt = sys.stdin.read().splitlines()
+firstLine = inpt[0]
 pizza_list = []
-if firstLine:
-    pizzaTotal, grpOne, grpTwo, grpThree = firstLine.split(" ")
-    
-for line in itertools.islice(inputFile, 0, int(firstLine[0])):
+if firstLine :
+    pizzaTotal, grpOne, grpTwo, grpThree = firstLine.split()    
+for line in itertools.islice(inpt, 1, int(pizzaTotal)) :
     pizza_list.append(line.strip())
-   
-
