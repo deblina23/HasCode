130f57d5aacac9ff40cc9244cf4fdbaf0663f3ea
deblina23 <deblina.ghosh.kolkata@gmail.com>
2021-02-07 15:59:24 +0000

    listLogicAdded

--- PizzaPractice/readInput.py
+++ PizzaPractice/readInput.py
@@ -1,13 +1,61 @@
 #!/usr/bin/env python3
-import itertools
-inputFile = input("Enter the File:")
-inputFile = open(inputFile,"r")
-firstLine = inputFile.readline()
-pizza_list = []
-if firstLine:
-    pizzaTotal, grpOne, grpTwo, grpThree = firstLine.split(" ")
+import math
+def readInput():
+    pizzaDetails = []
+    pizzaTotal, groupOne,groupTwo,groupThree = map(int,input().split())
+    for value  in range(0,pizzaTotal):
+            pizzaDetails.append(input())
+    return( pizzaTotal,groupOne,groupTwo,groupThree,pizzaDetails)
+
+def selectTeam(processInput):
+    groupList = []
+    pizzaTotal = processInput[0]
+    groupList = list(processInput[1:])
+    print(groupList)
+    groupOneTotalMember = groupList[0] * 2
+    groupTwoTotalMember = groupList[1]* 3
+    groupThreeTotalMember = groupList[2] * 4
+
+    if(pizzaTotal>=(groupOneTotalMember+groupTwoTotalMember+groupThreeTotalMember)):
+            return(groupList)
+    else:
+         if(pizzaTotal>9):
+             allTeamSet = math.floor(pizzaTotal/9)
+             excessPizza = pizzaTotal%9
+             if(allTeamSet <=min (groupList)):
+
+                 groupList = [x - allTeamSet for x in groupList]
+                 calculateLess(excessPizza,groupList)
+             else:
+                difference = allTeamSet - min(groupList)
+                allTeamSet = allTeamSet -difference
+                excessPizza = excessPizza + (difference*9)
+                groupList = [x - allTeamSet for x in groupList]
+                positionToChange = groupList.index(min(groupList))
+                groupList.remove(groupList[positionToChange])
+                groupList.insert(positionToChange,0)
+                print(groupList)
+                calculateLess(pizzaTotal, groupList)
+
+
+      else:
+            calculateLess(pizzaTotal, groupList)
+
+def calculateLess(pizzaTotal, groupList):
+    remainPizzaList = []
     
-for line in itertools.islice(inputFile, 0, int(firstLine[0])):
-    pizza_list.append(line.strip())
+
+    while(pizzaTotal>2 and member<=4):
+            pizzaTotal = pizzaTotal-member
+            remainPizzaList.append(member)
+            member = member+1
+
+    print(remainPizzaList)
+        
+        
+            
    
+if __name__ == "__main__":
+    processInput = readInput()
+    selectTeam(processInput[0:4])
 
